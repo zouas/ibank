9de331e521b5178ea4e82ba5088346d83055264d
Zouascur <zouascur@mail.ru>
2021-08-05 15:37:22 +0300

    code add fetch

--- js/app.js
+++ js/app.js
@@ -1,5 +1,86 @@
 'use strict';
 const API_URL = 'http://localhost:9999/api';
+/*
+const request = (method, url) => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = (evt) => resolve(evt);
+        xhr.onerror = (evt) => reject(evt);
+        xhr.send();
+    })
+};
+*/
+
+const showLoader = (parentEl) => {
+    parentEl.innerHTML = `
+<div class="loading-indicator"></div>
+`;
+}
+
+const loadAccount = (parentEl, data, err) => {
+    if (err) {
+        parentEl.innerHTML = `
+        <div>Произошла ошибка.</div>
+        `;
+        return;
+    }
+    console.log(data);
+    parentEl.innerHTML = `
+    <div class = "name">${data.account.name}</div>
+    <div class = "number">${data.account.number}</div>
+    <div class="balance"><span class="amount">${data.account.amount}</span> ₽</div>
+    `;
+}
+
+
+const accountsEl = document.getElementById('accounts-and-card');
+showLoader(accountsEl);
+
+
+fetch(`${API_URL}/hw15`)
+
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('err.server');
+        }
+        return response.json();
+        //const data = JSON.parse(evt.target.responseText);
+        //loadAccount(accountsEl, data);
+    })
+    .then((data) => {
+            loadAccount(accountsEl, data)
+        }
+    )
+    .catch((error) => {
+        loadAccount(accountsEl, null, 'err.common');
+    });
+
+
+/*
+
+request('GET', `${API_URL}/hw15`)
+
+    .then((evt) => {
+        if (evt.target.status !== 200) {
+            throw new Error('err.server');
+            //loadAccount(accountsEl, null, 'err.server');
+            //return;
+        }
+        const data = JSON.parse(evt.target.responseText);
+        loadAccount(accountsEl, data);
+    })
+    .catch(() => {
+        loadAccount(accountsEl, null, 'err.common');
+    }); */
+
+
+/*
+
+
+
+'use strict';
+const API_URL = 'http://localhost:9999/api';
 
 const request = (method, url, onload, onerror) => {
     const xhr = new XMLHttpRequest();
@@ -19,7 +100,7 @@ const showLoader = (parentEl) => {
 const loadAccount = (parentEl, data, err) => {
     if (err) {
         parentEl.innerHTML = `
-        <div>Произошла ошибка.</div>>
+        <div>Произошла ошибка.</div>
         `;
         return;
     }
@@ -36,9 +117,6 @@ const accountsEl = document.getElementById('accounts-and-card');
 showLoader(accountsEl);
 
 
-/*const xhr = new XMLHttpRequest();
-xhr.open('GET', `${API_URL}/hw15`);*/
-
 request('GET', `${API_URL}/hw15`,
     (evt) => {
         if (evt.target.status !== 200) {
@@ -54,13 +132,9 @@ request('GET', `${API_URL}/hw15`,
 
 );
 
-/*xhr.onload = (evt) => {
-
-};*/
-/*xhr.onerror = (evt) => {
-
-};*/
+*/
 
 //xhr.send();
 //const response = xhr.responseText;
 //console.log(response);
+
